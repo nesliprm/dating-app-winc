4cc2b340a8bef5b1b326573657c945e4dc7bd1e7
Nesli <nesliparma@gmail.com>
2025-03-13 17:16:08 +0100

    added empty profile

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+// const mockData = {
+//   data: [
+//     {
+//       first_name: "Ryun",
+//       last_name: "Cobb",
+//       age: 22,
+//       gender: "F",
+//       gender_interest: "M",
+//       location: "city",
+//       min_age_interest: 32,
+//       max_age_interest: 82,
+//     },
+//   ],
+// };
+let i = 55;
+let userFirstName = mockData.data[i].first_name;
+let userLastName = mockData.data[i].last_name;
+let userAge = mockData.data[i].age;
+let userGender = mockData.data[i].gender;
+let userGenderInterest = mockData.data[i].gender_interest;
+let userLocation = mockData.data[i].location;
+let userMinAgeInterest = mockData.data[i].min_age_interest;
+let userMaxAgeInterest = mockData.data[i].max_age_interest;
 
-
-console.log(mockData.data);
-
+const userProfile = {
+  first_name: userFirstName,
+  last_name: userLastName,
+  age: userAge,
+  gender: userGender,
+  gender_interest: userGenderInterest,
+  location: userLocation,
+  min_age_interest: userMinAgeInterest,
+  max_age_interest: userMaxAgeInterest,
+};
