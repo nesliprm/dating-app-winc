4b9f63d9c37960522f75b786824cdca741b6f88b
Nesli <nesliparma@gmail.com>
2025-03-13 17:49:30 +0100

    finished prompt questions

--- index.js
+++ index.js
@@ -12,23 +12,44 @@
 //     },
 //   ],
 // };
-let i = 55;
-let userFirstName = mockData.data[i].first_name;
-let userLastName = mockData.data[i].last_name;
-let userAge = mockData.data[i].age;
-let userGender = mockData.data[i].gender;
-let userGenderInterest = mockData.data[i].gender_interest;
-let userLocation = mockData.data[i].location;
-let userMinAgeInterest = mockData.data[i].min_age_interest;
-let userMaxAgeInterest = mockData.data[i].max_age_interest;
+// let i = 55;
+// let userFirstName = mockData.data[i].first_name;
+// let userLastName = mockData.data[i].last_name;
+// let userAge = mockData.data[i].age;
+// let userGender = mockData.data[i].gender;
+// let userGenderInterest = mockData.data[i].gender_interest;
+// let userLocation = mockData.data[i].location;
+// let userMinAgeInterest = mockData.data[i].min_age_interest;
+// let userMaxAgeInterest = mockData.data[i].max_age_interest;
 
-const userProfile = {
-  first_name: userFirstName,
-  last_name: userLastName,
-  age: userAge,
-  gender: userGender,
-  gender_interest: userGenderInterest,
-  location: userLocation,
-  min_age_interest: userMinAgeInterest,
-  max_age_interest: userMaxAgeInterest,
-};
+const yourProfile = {};
+
+let yourName = prompt("What is your name?");
+yourProfile.first_name = yourName;
+
+let yourLastName = prompt("What is your last name?");
+yourProfile.last_name = yourLastName;
+
+let yourAge = prompt("What is your age?");
+yourProfile.age = yourAge;
+
+let yourGender = prompt("What is your gender? (Please enter F, M or X)");
+yourProfile.gender = yourGender;
+
+let yourGenderInterest = prompt(
+  "Which gender are you interested in? (Please enter F, M or X)"
+);
+yourProfile.gender_interest = yourGenderInterest;
+
+let yourLocation = prompt("What is your location?");
+yourProfile.location = yourLocation;
+
+let yourMinAgeInterest = prompt(
+  "What is the minimum age you're interested in?"
+);
+yourProfile.min_age_interest = yourMinAgeInterest;
+
+let yourMaxAgeInterest = prompt(
+  "What is the maximum age you're interested in?"
+);
+yourProfile.max_age_interest = yourMaxAgeInterest;
